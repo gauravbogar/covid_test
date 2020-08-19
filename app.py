4135843891d8f803aec6c63683fcbb3f44c58de9
Gaurav Bogar <gauravbogar@gmail.com>
2020-08-19 12:09:34 +0530

    auto pep8 formatting

--- app.py
+++ app.py
@@ -1,60 +1,89 @@
-from flask import jsonify, request, Flask
-import pandas as pd
-import numpy as np 
 import os
 import pickle
+
+import numpy as np
+import pandas as pd
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 current_path = os.getcwd()
-pickle_path = os.path.join(current_path, 'assets', 'covid.pkl')
+pickle_path = os.path.join(current_path, "assets", "covid.pkl")
 print(pickle_path)
 classifier = pickle.load(open(pickle_path, "rb"))
-# with open(pickle_path, 'wb') as f:
-#     classifier = pickle.load(f)
 
-@app.route('/api/covid_test', methods=['POST'])
+
+@app.route("/api/covid_test", methods=["POST"])
 def predict_risk():
     data = request.get_json()
-    sex = data['sex']
-    pneumonia = data['pneumonia']
-    age = data['age']
-    pregnancy = data['pregnancy']
-    diabetes = data['diabetes']
-    copd = data['copd']
-    asthma = data['asthma']
-    hypertension = data['hypertension']
-    other_disease = data['other_disease']
-    cardiovascular = data['cardiovascular']
-    obesity = data['obesity']
-    renal_chronic = data['renal_chronic']
-    tobacco = data['tobacco']
-    contact_other_covid = data['contact_other_covid']
+    sex = data["sex"]
+    pneumonia = data["pneumonia"]
+    age = data["age"]
+    pregnancy = data["pregnancy"]
+    diabetes = data["diabetes"]
+    copd = data["copd"]
+    asthma = data["asthma"]
+    hypertension = data["hypertension"]
+    other_disease = data["other_disease"]
+    cardiovascular = data["cardiovascular"]
+    obesity = data["obesity"]
+    renal_chronic = data["renal_chronic"]
+    tobacco = data["tobacco"]
+    contact_other_covid = data["contact_other_covid"]
 
-    cols = ['sex', 'pneumonia', 'age', 'pregnancy', 'diabetes', 'copd', 'asthma',
-       'hypertension', 'other_disease', 'cardiovascular', 'obesity',
-       'renal_chronic', 'tobacco', 'contact_other_covid']
-    
-    test = pd.DataFrame([[sex, pneumonia, age, pregnancy, diabetes, copd, asthma,
-       hypertension, other_disease, cardiovascular, obesity,
-       renal_chronic, tobacco, contact_other_covid,]], columns = cols)
-    
-    
+    cols = [
+        "sex",
+        "pneumonia",
+        "age",
+        "pregnancy",
+        "diabetes",
+        "copd",
+        "asthma",
+        "hypertension",
+        "other_disease",
+        "cardiovascular",
+        "obesity",
+        "renal_chronic",
+        "tobacco",
+        "contact_other_covid",
+    ]
+
+    test = pd.DataFrame(
+        [
+            [
+                sex,
+                pneumonia,
+                age,
+                pregnancy,
+                diabetes,
+                copd,
+                asthma,
+                hypertension,
+                other_disease,
+                cardiovascular,
+                obesity,
+                renal_chronic,
+                tobacco,
+                contact_other_covid,
+            ]
+        ],
+        columns=cols,
+    )
 
     pred = classifier.predict_proba(test)
     result = pred[0][1]
 
     if result < 0.25:
-        return jsonify('Low Risk')
+        return jsonify("Low Risk")
     elif (result >= 0.25) and (result < 0.5):
-        return jsonify('Medium Risk')
+        return jsonify("Medium Risk")
 
     elif (result >= 0.5) and (result < 0.75):
-        return jsonify('High Risk')
+        return jsonify("High Risk")
 
     else:
-        return jsonify('Vulnerable Risk')
+        return jsonify("Vulnerable Risk")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
@@ -10,22 +10,27 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingClassifier
 
 current_path = os.getcwd()
-data_path = os.path.join(current_path, 'assets', 'covid.csv')
-pickle_path = os.path.join(current_path, 'assets', 'covid.pkl')
+data_path = os.path.join(current_path, "assets", "covid.csv")
+pickle_path = os.path.join(current_path, "assets", "covid.pkl")
 
 
 def train(df, x, y):
     global x_train, x_test, y_train, y_test
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
-    cat_feat = [feature for feature in x.columns if df[feature].dtypes == 'O']
-    num_feat = [feature for feature in x.columns if df[feature].dtypes != 'O']
+    cat_feat = [feature for feature in x.columns if df[feature].dtypes == "O"]
+    num_feat = [feature for feature in x.columns if df[feature].dtypes != "O"]
 
-
-    col_transformer = ColumnTransformer(transformers= [('ohe', OneHotEncoder(drop='first'), cat_feat),
-                                                   ('ss', MinMaxScaler(), num_feat)])
+    col_transformer = ColumnTransformer(
+        transformers=[
+            ("ohe", OneHotEncoder(drop="first"), cat_feat),
+            ("ss", MinMaxScaler(), num_feat),
+        ]
+    )
     model = GradientBoostingClassifier()
-    pipe = Pipeline([("preprocessing", col_transformer), ('pca', PCA()),("model", model)])
+    pipe = Pipeline(
+        [("preprocessing", col_transformer), ("pca", PCA()), ("model", model)]
+    )
     pipe.fit(x_train, y_train)
     # pickle.dump(pipe, open(pickle_path, 'wb'))
     # print('Pickle file created at ', pickle_path)
@@ -42,20 +47,21 @@ def test(pickle_path):
     result = classifier.predict_proba(x_test.head(1))
     print(result)
     if result[0][1] < 0.25:
-        print ('Low Risk')
+        print("Low Risk")
     elif (result[0][1] >= 0.25) and (result[0][1] < 0.5):
-        print ('Medium Risk')
+        print("Medium Risk")
 
     elif (result[0][1] >= 0.5) and (result[0][1] < 0.75):
-        print ('High Risk')
+        print("High Risk")
 
     else:
-        print ('Vulnerable Risk')
+        print("Vulnerable Risk")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     df = pd.read_csv(data_path)
-    x = df.drop('covid_res', axis=1)
-    y = df['covid_res']
+    x = df.drop("covid_res", axis=1)
+    y = df["covid_res"]
     pickle_path = train(df, x, y)
     test(pickle_path)
